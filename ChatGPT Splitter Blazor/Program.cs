69849bfea774ebcf924aa7d8189e26482806b180
Gianluigi Salvi <g.salvi@lottomatica.com>
2023-08-28 10:31:44 +0200

    Initial commit

---
+++ ChatGPT Splitter Blazor/Program.cs
@@ -0,0 +1,11 @@
+using ChatGPT_Splitter_Blazor_New;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
