32260e723618c0c5a958bdd52575f0b314b85b5f
Gianluigi Salvi <gianluigi.salvi@gmail.com>
2023-09-27 19:31:34 +0200

    svil

--- ChatGPT Splitter Blazor/ChatGptClient.cs
+++ ChatGPT Splitter Blazor/ChatGptClient.cs
@@ -1,26 +1,62 @@
 ﻿using ChatGPT_Splitter_Blazor_New.Config;
+using Newtonsoft.Json;
 using OpenAI_API;
 using OpenAI_API.Chat;
 using OpenAI_API.Models;
+using System.Reflection;
+using Forge.OpenAI.Models.ChatCompletions;
+using Forge.OpenAI.Models.Common;
+using ChatMessage = Forge.OpenAI.Models.ChatCompletions.ChatMessage;
+using Forge.OpenAI.Interfaces.Services;
 
 namespace ChatGPT_Splitter_Blazor_New;
 
+public class ConfigurationLoader
+{
+    public static AppSettings LoadEmbeddedAppSettings()
+    {
+        const string resourceName = "ChatGPT_Splitter_Blazor_New.appsettings.json";
+        string content = ReadEmbeddedResourceContent(resourceName);
+        return JsonConvert.DeserializeObject<AppSettings>(content);
+    }
+
+    private static string ReadEmbeddedResourceContent(string resourceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        using Stream stream = assembly.GetManifestResourceStream(resourceName);
+        using StreamReader reader = new StreamReader(stream);
+
+        return reader.ReadToEnd();
+    }
+}
+
 public class ChatGptClient
 {
-    private readonly OpenAIAPI api;
+    private readonly OpenAIAPI _api;
+    private readonly IOpenAIService _openAiService;
+    private readonly string _apiKey;
 
-    public ChatGptClient()
+    public ChatGptClient(IOpenAIService openAiService)
     {
-        var config = AppSettings.Load("appsettings.json");
-        string OPENAI_API_KEY = config.OpenAI.ApiKey;
-        api = new OpenAIAPI(OPENAI_API_KEY);
+        // Due to the nature of Blazor WebAssembly (similar to JS), traditional server-side configuration files are not accessible.
+        // Blazor WebAssembly, in fact, run entirely within the client's browser. As a result, I've embedded the configuration as a 
+        // resource within the assembly to allow for its secure retrieval and usage without exposing sensitive details on public repositories (github).
+
+        //var config = AppSettings.Load("appsettings.json");
+        var settings = ConfigurationLoader.LoadEmbeddedAppSettings();
+        _apiKey = settings.OpenAI.ApiKey;
+        
+        _api = new OpenAIAPI(_apiKey);
+
+        _openAiService = openAiService;
     }
 
     public async Task<string> GenerateTextAsync(string userInput, string istruzioni, Model model)
     {
         //https://github.com/OkGoDoIt/OpenAI-API-dotnet#chat-conversations
         ChatRequest config = new ChatRequest(){Model = model};
-        var chat = api.Chat.CreateConversation(config);
+        var chat = _api.Chat.CreateConversation(config);
 
         /// give instruction as System
         chat.AppendSystemMessage(istruzioni);
@@ -33,4 +69,30 @@ public class ChatGptClient
 
         return response;
     }
+
+    public async Task<string> GenerateTextWithForgeAsync(string userInput, string istruzioni)
+    {
+        // Crea la richiesta iniziale
+        ChatCompletionRequest request = new ChatCompletionRequest(ChatMessage.CreateFromSystem(istruzioni));
+        request.Messages.Add(ChatMessage.CreateFromUser(userInput));
+
+        // Non sono sicuro di come tu stia gestendo 'Model' nel tuo codice attuale, 
+        // ma potresti doverlo incorporare nella richiesta qui, se necessario.
+
+        // Effettua la richiesta e ottieni la risposta
+        HttpOperationResult<ChatCompletionResponse> response =
+            await _openAiService.ChatCompletionService.GetAsync(request, CancellationToken.None).ConfigureAwait(false);
+
+        if (response.IsSuccess && response.Result != null && response.Result.Choices.Any())
+        {
+            return response.Result.Choices[0].Message.Content;
+        }
+        else
+        {
+            // Gestisci l'errore come preferisci
+            return "Errore durante l'ottenimento della risposta. " + response.ErrorMessage;
+        }
+    }
+
+
 }
--- ChatGPT Splitter Blazor/Program.cs
+++ ChatGPT Splitter Blazor/Program.cs
@@ -1,4 +1,7 @@
+using Serilog;
 using ChatGPT_Splitter_Blazor_New;
+using Forge.OpenAI;
+using Forge.OpenAI.Authentication;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,40 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Configuration for Serilog
+//var environmentName = builder.HostEnvironment.Environment;
+//var configuration = new ConfigurationBuilder()
+//    .AddJsonFile("appsettings.json")
+//    //.AddJsonFile($"appsettings.{environmentName}.json", true)
+//    .Build();
 
-await builder.Build().RunAsync();
+Log.Logger = new LoggerConfiguration()
+    //.ReadFrom.Configuration(configuration)
+    //.WriteTo.BrowserConsole()  // Log to browser console
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web host");
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    var settings = ConfigurationLoader.LoadEmbeddedAppSettings();
+    // Add the ForgeOpenAI service
+    builder.Services.AddForgeOpenAI(options =>
+    {
+        options.AuthenticationInfo = new AuthenticationInfo(settings.OpenAI.ApiKey);
+    });
+    builder.Services.AddScoped<ChatGptClient>();
+
+
+    await builder.Build().RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    throw;  // Consider whether you want to propagate the exception or handle it.
+}
+finally
+{
+    Log.CloseAndFlush();
+}
